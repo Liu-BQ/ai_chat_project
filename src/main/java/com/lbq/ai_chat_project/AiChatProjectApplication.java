057101bbe24e9174e7f096e30896779f57f7378f
LiuBQ <2812398586@qq.com>
2025-12-28 19:50:12 +0800

    init

---
+++ src/main/java/com/lbq/ai_chat_project/AiChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lbq.ai_chat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiChatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbq/ai_chat_project/AiChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbq.ai_chat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
